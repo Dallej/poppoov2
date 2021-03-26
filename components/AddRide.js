56eb43e74fe98d5e837f629e3e11dcbb6a1b0e94
Emma Määttälä <emmamaattala@Emmas-MacBook-Pro.local>
2021-03-26 12:03:12 +0200

    Nothing added

--- components/AddRide.js
+++ components/AddRide.js
@@ -36,9 +36,10 @@ export default class Login extends React.Component{
                 <Calendar
                 onChange={(date)=> this.setState({date})}
                 selected={this.state.date}
-                minDate={Moment().startOf('day')}
-                maxDate={Moment().add(10,'years').startOf('day')}
+                 minDate={Moment().startOf('day')}
+                 maxDate={Moment().add(10,'years').startOf('day')}
                 />
+                
                 
                {/* <Text>Hello, {firebase.auth().currentUser.email}</Text> */}
 
