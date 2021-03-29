2e86d94d53c7bd65166b53285ca2130776d4e458
JMoilanen <57660263+JMoilanen@users.noreply.github.com>
2021-03-29 10:48:23 +0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,73 +1,21 @@
-
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import styles from './style/style'
-import Menu from './components/Menu'
-import AddRide from './components/AddRide'
-import Rides from './components/Rides'
-import Login from './components/Login'
-
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import Info from './components/Info';
-
-
-const Stack = createStackNavigator();
-
-
-export default class App extends React.Component {
-  render(){
-  
-  
+export default function App() {
   return (
-
-    <NavigationContainer >
-        <Stack.Navigator initialRouteName="Menu">
-          <Stack.Screen name="menu" component={Menu}
-          options={{
-            title:"poppoo",
-            headerTitle:"poppoo"
-          }}
-          >
-          </Stack.Screen>
-
-          <Stack.Screen name="rides" component={Rides}
-          options={{
-            title:"Available rides",
-            headerTitle:"Available rides"
-          }}
-          >
-          </Stack.Screen>
-
-          <Stack.Screen name="addride" component={AddRide}
-          options={{
-            title:"Offer a ride",
-            headerTitle:"Offer a ride"
-          }}
-          >
-          </Stack.Screen>
-
-          <Stack.Screen name="info" component={Info}
-          options={{
-            title:"Info",
-            headerTitle:"Info"
-          }}
-          >
-          </Stack.Screen>
-
-          <Stack.Screen name="login" component={Login}
-          options={{
-            title:"Login",
-            headerTitle:"Login"
-          }}
-          >
-          </Stack.Screen>
-        </Stack.Navigator>
-    </NavigationContainer>
-    
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-}
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
