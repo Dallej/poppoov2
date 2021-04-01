7f66829c04616a7b5908afe550da20d015743eb9
Dallej <57659831+Dallej@users.noreply.github.com>
2021-04-01 12:15:40 +0300

    add ride changes

--- components/AddRide.js
+++ components/AddRide.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Pressable, Text, TextInput} from 'react-native'
+import {View, Pressable, Text, TextInput,TouchableWithoutFeedback} from 'react-native'
 import Calendar from 'react-native-calendar-datepicker';
 import Moment from 'moment';
 
@@ -7,6 +7,7 @@ import {firebase} from '../firebase/Config'
 require('firebase/auth')
 
 import styles from '../style/style'
+import {Keyboard} from 'react-native'
 
 
 
@@ -19,7 +20,8 @@ export default class AddRide extends React.Component {
         super();
         this.state = {
             name: '',
-            time: ''
+            time: '',
+            mobile:''
         }
     
     }
@@ -28,10 +30,10 @@ export default class AddRide extends React.Component {
       AddToDatabase = () => {
         const {name,time,date} = this.state
         firebase.auth()
-        // .createUserWithEmailAndPassword(email, password)
         .then(() => firebase.database().ref(ROOT_REF).push({
             name: name,
-            time:time
+            time:time,
+            mobile:mobile
         }))
         .then(() => this.props.navigation.navigate('rides'))
         .catch(error => console.log(error))
@@ -39,8 +41,9 @@ export default class AddRide extends React.Component {
 
     render() {
         return (
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View style={styles.container}>
-
+                
                 <Text style={styles.headertext}>Enter new ride information:</Text>
 
                 <Text style={styles.infotext}>Driver name</Text>
@@ -57,15 +60,14 @@ export default class AddRide extends React.Component {
 
                 <TextInput 
                     style={styles.inputBox} 
-                    value={this.state.name}
-                    onChangeText={name=> this.setState({name})}
+                    value={this.state.mobile}
+                    onChangeText={mobile=> this.setState({mobile})}
                     placeholder='enter your mobile number...'
                     autoCapitalize='none'
+                    keyboardType="phone-pad"
                     autocomplete="tel">
                     
                 </TextInput>
-
-                
 
                 <Text style={styles.infotext}>Enter Time </Text>
 
@@ -78,26 +80,26 @@ export default class AddRide extends React.Component {
                     keyboardType="decimal-pad"
                     type="time"> 
                 </TextInput>
+    
 
-                
 
+
+                <Pressable style={styles.button} onPress={this.AddToDatabase}>
+                    <Text style={styles.buttonText}>Add </Text>
+                </Pressable>
 
                 <Text style={styles.infotext}>Select date </Text>
+
                 <Calendar
                 onChange={(date)=> this.setState({date})}
                 selected={this.state.date}
                  minDate={Moment().startOf('day')}
                  maxDate={Moment().add(10,'years').startOf('day')}
                 />
-                
-                
-               {/* <Text>Hello, {firebase.auth().currentUser.email}</Text> */}
 
-                
-                <Pressable style={styles.button} onPress={this.AddToDatabase}>
-                    <Text style={styles.buttonText}>Add </Text>
-                </Pressable>
             </View>
+            </TouchableWithoutFeedback>
+            
         )
     }
 
--- components/Info.js
+++ components/Info.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import {View, TextInput, Pressable, Text, Image} from 'react-native'
-import {firebase, ROOT_REF} from '../firebase/Config'
+import { View, TextInput, Pressable, Text, Image } from 'react-native'
+import { firebase, ROOT_REF } from '../firebase/Config'
 import styles from '../style/style'
 
 
-export default class Info extends React.Component{
+export default class Info extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            name:'',
-            email:'',
-            password:''
-        }    
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
     }
 
-    render(){
-        return(
-            <View style={[styles.container,styles.centerpadding]}>
-                <Image style={styles.logo} source={require("../img/OAMKlogo.png")}></Image> 
-                
-                <Text style={{fontWeight: "bold", color: "#fff"}}>OAMK Oulu pickup location</Text>
-                <Text style={{color: "#fff"}}> Yliopistokatu 9, 90570 Oulu </Text>
-            <Image style={styles.map} source={require("../img/mapfirst.jpg")}></Image> 
-            
-                <Text style={{fontWeight: "bold", color: "#fff"}}>OAMK Oulainen pickup location</Text>
-                <Text  style={{color: "#fff"}}> Kuntotie 2, 86300 Oulainen</Text>
-                {/* <Pressable style={styles.button} onPress={this.signUp}>
-                    <Text style={styles.buttonText}>Signup</Text>
-                </Pressable> */}
+    render() {
+        return (
+            <View style={[styles.container, styles.centerpadding]}>
+                <Image style={styles.logo} source={require("../img/OAMKlogo.png")}></Image>
+
+
+                <View>
+                    <Text style={{ fontWeight: "bold", color: "#fff" }}>OAMK Oulu pickup location</Text>
+                    <Text style={{ color: "#fff" }}> Yliopistokatu 9, 90570 Oulu </Text>
+                    <Image style={styles.map} source={require("../img/mapfirst.jpg")}></Image>
+                </View>
+
+                <View>
+                    <Text style={{ fontWeight: "bold", color: "#fff" }}>OAMK Oulainen pickup location</Text>
+                    <Text style={{ color: "#fff" }}> Kuntotie 2, 86300 Oulainen</Text>
+                    <Image style={styles.map} source={require("../img/mapfirst.jpg")}></Image>
+                </View>
             </View>
         )
     }
