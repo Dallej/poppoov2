72198e72b01f2567f724644063df29330c8be999
Jjohannesaf <n9ahjo00@students.oamk.fi>
2021-04-21 10:19:44 +0300

    -
    nothing added

--- components/Onboarding.js
+++ components/Onboarding.js
@@ -1,55 +1,56 @@
-import { Image } from 'react-native';
-import React from 'react';
+ import { Image } from 'react-native';
+ import React from 'react';
 
-import Onboarding from 'react-native-onboarding-swiper';
+ import Onboarding from 'react-native-onboarding-swiper';
 
 
 
-/* const Simple = () => ( */
-
- export default class Simple extends React.Component{  
+/*  const Simple = () => ( 
+ */
+  export default class Simple extends React.Component{  
   
-  constructor(){
-    super();
-    this.state={
+   constructor(){
+     super();
+     this.state={
         
-    }   
-}
+     }   
+ }
 
-  render(){
+   render(){
     
-    return ( 
+     return ( 
 
-    <Onboarding
-    onDone={() =>  this.props.navigation.navigate('menu')}
-    onSkip={() => this.props.navigation.navigate('menu')}
-    bottomBarHeight={60}
-    topBarHeight={0}
-    imageContainerStyles={{height:400,width:10,paddingTop:10}}
-    containerStyles={{height:0}}
-    pages={[
-      {
-        backgroundColor: '#fff',
+     <Onboarding
+     onDone={() =>  this.props.navigation.navigate('menu')}
+     onSkip={() => this.props.navigation.navigate('menu')}
+     bottomBarHeight={60}
+     topBarHeight={0}
+     imageContainerStyles={{height:400,width:10,paddingTop:10}}
+     containerStyles={{height:0}}
+     pages={[
+       {
+         backgroundColor: '#fff',
         
-        title: 'Welcome to POPPOO',
-        image: <Image source={require('../img/kuva.png')} />,
-        subtitle: 'Start by checking out rides...',
-      },
-      {
-        backgroundColor: '#f1c11a',
+         title: 'Welcome to POPPOO',
+         image: <Image source={require('../img/kuva.png')} />,
+         subtitle: 'Start by checking out rides...',
+       },
+       {
+         backgroundColor: '#f1c11a',
        
-        title: 'How to use...',
-        image: <Image source={require('../img/kuva.png')} />,
-        subtitle: 'Continue by joining a ride...',
-      },
-      {
-        backgroundColor: '#999',
-        title: 'Enjoy your free trip!',
-        image: <Image source={require('../img/kuva.png')} />,
-        subtitle: 'Remember to stay safe!',
-      },
-    ]}
-  />
-);
+         title: 'How to use...',
+         image: <Image source={require('../img/kuva.png')} />,
+         subtitle: 'Continue by joining a ride...',
+       },
+       {
+         backgroundColor: '#999',
+         title: 'Enjoy your free trip!',
+         image: <Image source={require('../img/kuva.png')} />,
+         subtitle: 'Remember to stay safe!',
+       },
+     ]}
+    />
+ );
   
-  }}
+   }}
+ 
