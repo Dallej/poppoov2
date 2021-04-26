2f64db5a86dc97a57e6df7c12fb54dc145e81bff
elise laurila <n9lael00@students.oamk.fi>
2021-04-26 11:38:50 +0300

    onboarding colors

--- components/Onboarding.js
+++ components/Onboarding.js
@@ -29,30 +29,38 @@
      containerStyles={{height:0}}
      pages={[
        {
-         backgroundColor: '#fff',
+         backgroundColor: '#77AB9F',
         
         title: 'Welcome to OAMK POPPOO',
+        titleStyles: {color: "#fff"}, 
+        subTitleStyles: {color: "#fff"},
         image: <Image source={require('../img/createAccount.png')} />,
         subtitle: 'Start by creating an account',
       },
       {
-        backgroundColor: '#fff',
+        backgroundColor: '#1c1c1c',
        
         title: 'Check out available rides or create your own',
         image: <Image source={require('../img/JoinRideImage.png')} />,
         subtitle: 'Join a ride or offer one',
+        titleStyles: {color: "#fff"}, 
+        subTitleStyles: {color: "#fff"},
       },
       {
-        backgroundColor: '#fff',
+        backgroundColor: '#1c1c1c',
         title: 'Enjoy your free trip!',
         image: <Image source={require('../img/tripImage.png')} />,
         subtitle: 'Remember to thank the driver',
+        titleStyles: {color: "#fff"}, 
+        subTitleStyles: {color: "#fff"},
       },
       {
-        backgroundColor: '#fff',
+        backgroundColor: '#1c1c1c',
         title: 'Remember to stay safe!',
         image: <Image source={require('../img/staySafe.png')} />,
         subtitle: 'More COVID-19 info: www.thl.fi',
+        titleStyles: {color: "#fff"}, 
+        subTitleStyles: {color: "#fff"},
       },
     ]}
   />
